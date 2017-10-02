37a84bc0b23f0d8f33786b3ff3e6d533b471af76
Ahmet ÖZTÜRK <rawsludge@hotmail.com>
2017-10-02 22:35:50 +0300

    Added JPA repository and implemented first

---
+++ src/main/java/net/mobilim/NaviGateService/Interfaces/ProductRepository.java
@@ -0,0 +1,16 @@
+package net.mobilim.NaviGateService.Interfaces;
+
+import net.mobilim.NaviGateData.Entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Date;
+
+@Transactional
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+    @Query("SELECT p FROM Product p WHERE p.sailingDate BETWEEN ? AND ?")
+    Product findBySailingDate(Date fromDate, Date toDate);
+}
--- src/main/java/net/mobilim/NaviGateService/ServiceApp.java
+++ src/main/java/net/mobilim/NaviGateService/ServiceApp.java
@@ -1,15 +1,27 @@
 package net.mobilim.NaviGateService;
 
+import net.mobilim.NaviGateData.Entities.Destination;
+import net.mobilim.NaviGateData.Entities.Port;
+import net.mobilim.NaviGateData.Entities.Product;
+import net.mobilim.NaviGateData.Entities.Ship;
+import net.mobilim.NaviGateService.Interfaces.ProductRepository;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@EntityScan("net.mobilim.NaviGateData.Entities")
 public class ServiceApp {
     private Logger _logger = LogManager.getLogger(ServiceApp.class);
+
+    @Value("{website.url}")
+    private String websiteUrl;
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = new SpringApplicationBuilder()
@@ -20,7 +32,20 @@ public class ServiceApp {
         app.start();
     }
 
+    @Autowired
+    private ProductRepository productRepository;
+
     private void start() {
         _logger.info("Application started");
+        Product product = new Product();
+        Ship ship = new Ship("PA", "Princes");
+        product.setShip(ship);
+        Destination dest = new Destination("LA", "Losangels");
+        product.setDestination(dest);
+        Port port = new Port("FLL", "Ft. Lauderdale, Florida");
+        product.setEmbarkPort(port);
+        port = new Port("FLL", "Ft. Lauderdale, Florida");
+        product.setDebarkPort(port);
+        productRepository.save(product);
     }
 }

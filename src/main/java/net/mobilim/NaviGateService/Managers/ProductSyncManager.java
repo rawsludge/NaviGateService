e538fde4c17c9a55d8dc1c9741296d8db4363b5c
Ahmet ÖZTÜRK <rawsludge@hotmail.com>
2017-10-04 23:13:28 +0300

    Updated ProductSyncManager class.

--- src/main/java/net/mobilim/NaviGateService/Managers/ProductSyncManager.java
+++ src/main/java/net/mobilim/NaviGateService/Managers/ProductSyncManager.java
@@ -1,10 +1,14 @@
 package net.mobilim.NaviGateService.Managers;
 
-import net.mobilim.Repositories.Entities.Destination;
-import net.mobilim.Repositories.Repositories.DestinationRepository;
-import net.mobilim.Repositories.Repositories.PortRepository;
-import net.mobilim.Repositories.Repositories.ProductRepository;
-import net.mobilim.Repositories.Repositories.ShipRepository;
+
+import net.mobilim.NaviGateData.Entities.Destination;
+import net.mobilim.NaviGateData.Entities.Port;
+import net.mobilim.NaviGateData.Entities.Product;
+import net.mobilim.NaviGateData.Entities.Ship;
+import net.mobilim.NaviGateData.Repositories.DestinationRepository;
+import net.mobilim.NaviGateData.Repositories.PortRepository;
+import net.mobilim.NaviGateData.Repositories.ProductRepository;
+import net.mobilim.NaviGateData.Repositories.ShipRepository;
 import net.mobilim.NaviGateService.Helpers.XmlDefinitions;
 import net.mobilim.NaviGateService.HttpWebRequest;
 import net.mobilim.NaviGateService.ServiceApp;
@@ -15,6 +19,7 @@ import org.json.JSONObject;
 import org.json.XML;
 import org.springframework.beans.factory.annotation.Value;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -55,17 +60,64 @@ public class ProductSyncManager {
             logger.error(e);
             throw  e;
         }
+
         for (Object item:jsonArray) {
             if( !(item instanceof JSONObject))  continue;
 
             jsonObject = (JSONObject)item;
 
-            JSONObject tempObject = jsonObject.getJSONObject("Destination");
-            String code = tempObject.getString("Code");
-            String name = tempObject.getString("Name");
-            Destination destination = destinationRepository. checkDestinationAndSave(code, name);
+            Destination destination = prepateDestionation(jsonObject.getJSONObject("Destination"));
+            Port embarkPort = preparePort(jsonObject.getJSONObject("EmbarkPort"));
+            Port debarkPort = preparePort(jsonObject.getJSONObject("DebarkPort"));
+            JSONObject tempJsonObject = jsonObject.getJSONObject("Sailing");
+            Ship ship = prepareShip(tempJsonObject.getJSONObject("Ship"));
 
+            Integer duration = tempJsonObject.getInt("DurationDays");
+            Date sailingDate = null;
+            try {
+                sailingDate = dateFormatter.parse( tempJsonObject.get("Date").toString() );
+            } catch (ParseException e) {
+                logger.error(e);
+                throw e;
+            }
+
+            String sailingID = jsonObject.get("SailingId").toString();
+            Integer maxOccupancy = jsonObject.getInt("MaxOccupancy");
+
+            Product product = new Product();
+            product.setDestination(destination);
+            product.setEmbarkPort(embarkPort);
+            product.setShip(ship);
+            product.setDuration(duration);
+            product.setSailingDate(sailingDate);
+            product.setMaxOccupancy(maxOccupancy);
+            product.setDebarkPort(debarkPort);
+            product.setSailingID(sailingID);
+            product.setCruiseLineCode("PCL");
+            product.setLastUpdateDate(date);
+            productRepository.save(product);
         }
 
+    }
+
+    private Destination prepateDestionation(JSONObject jsonObject) {
+        String code = jsonObject.getString("Code");
+        String name = jsonObject.getString("Name");
+        Destination destination = destinationRepository.checkAndSave(code, name);
+        return  destination;
+    }
+
+    private Port preparePort(JSONObject jsonObject) {
+        String code = jsonObject.getString("Code");
+        String name = jsonObject.getString("Name");
+        Port port = portRepository.checkAndSave(code, name);
+        return port;
+    }
+
+    private Ship prepareShip(JSONObject jsonObject) {
+        String code = jsonObject.getString("Code");
+        String name = jsonObject.getString("Name");
+        Ship ship = shipRepository.checkAndSave(code, name);
+        return ship;
     }
 }
--- src/main/java/net/mobilim/NaviGateService/ServiceApp.java
+++ src/main/java/net/mobilim/NaviGateService/ServiceApp.java
@@ -1,13 +1,6 @@
 package net.mobilim.NaviGateService;
 
-import net.mobilim.Repositories.Entities.Destination;
-import net.mobilim.Repositories.Entities.Port;
-import net.mobilim.Repositories.Entities.Product;
-import net.mobilim.Repositories.Entities.Ship;
-import net.mobilim.Repositories.Repositories.DestinationRepository;
-import net.mobilim.Repositories.Repositories.PortRepository;
-import net.mobilim.Repositories.Repositories.ProductRepository;
-import net.mobilim.Repositories.Repositories.ShipRepository;
+
 import net.mobilim.NaviGateService.Helpers.XmlDefinitions;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
@@ -41,17 +34,6 @@ public class ServiceApp {
         app.start(args);
     }
 
-    @Autowired
-    private ProductRepository productRepository;
-
-    @Autowired
-    private PortRepository portRepository;
-
-    @Autowired
-    private ShipRepository shipRepository;
-
-    @Autowired
-    private DestinationRepository destinationRepository;
 
     private void start(String[] args) {
         StringBuilder sb = new StringBuilder();
@@ -74,89 +56,5 @@ public class ServiceApp {
 
         String xmlPostData = String.format( XmlDefinitions.PRODUCT, fromDate, toDate, "5", "30", "", "");
 
-        HttpWebRequest webRequest = HttpWebRequest.Create(websiteUrl);
-        String response = "";
-        try {
-            response = webRequest.Post(xmlPostData);
-        } catch (Exception e) {
-            logger.error(e);
-            return;
-        }
-        JSONObject jsonObject = XML.toJSONObject(response.toString());
-
-        for ( Object object : jsonArray ) {
-            if( object instanceof JSONObject) {
-                logger.info(object);
-                JSONObject productJsonObject = (JSONObject)object;
-
-                tempObject = productJsonObject.getJSONObject("EmbarkPort");
-                code = tempObject.getString("Code");
-                name = tempObject.getString("Name");
-                Port embarkPort = checkPortAndSave(code, name);
-
-                tempObject = productJsonObject.getJSONObject("Sailing");
-                code = tempObject.getJSONObject("Ship").getString("Code");
-                name = tempObject.getJSONObject("Ship").getString("Name");
-                Ship ship = checkShipAndSave(code, name);
-
-                Integer duration = tempObject.getInt("DurationDays");
-                Date sailingDate = null;
-                try {
-                    sailingDate = dateFormat.parse( tempObject.get("Date").toString() );
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-
-                tempObject = productJsonObject.getJSONObject("DebarkPort");
-                code = tempObject.getString("Code");
-                name = tempObject.getString("Name");
-                Port debarkPort = checkPortAndSave(code, name);
-
-                String sailingID = productJsonObject.get("SailingId").toString();
-                Integer maxOccupancy = productJsonObject.getInt("MaxOccupancy");
-                Product product = new Product();
-                product.setDestination(destination);
-                product.setEmbarkPort(embarkPort);
-                product.setShip(ship);
-                product.setDuration(duration);
-                product.setSailingDate(sailingDate);
-                product.setMaxOccupancy(maxOccupancy);
-                product.setDebarkPort(debarkPort);
-                product.setSailingID(sailingID);
-                product.setCruiseLineCode("PCL");
-                product.setLastUpdateDate(date);
-                productRepository.save(product);
-            }
-        }
-    }
-
-    private Ship checkShipAndSave(String code, String name) {
-        Ship ship = shipRepository.findByCode(code);
-        if( ship == null) {
-            ship = new Ship(code, name);
-            ship.setLastUpdateDate(new Date());
-            shipRepository.save(ship);
-        }
-        return ship;
-    }
-
-    private Port checkPortAndSave(String code, String name) {
-        Port port = portRepository.findByCode(code);
-        if(port == null) {
-            port = new Port(code, name);
-            port.setLastUpdateDate(new Date());
-            portRepository.save(port);
-        }
-        return port;
-    }
-
-    private Destination checkDestinationAndSave(String code, String name) {
-        Destination destination = destinationRepository.findByCode(code);
-        if(destination == null) {
-            destination = new Destination(code, name);
-            destination.setLastUpdateDate(new Date());
-            destinationRepository.save(destination);
-        }
-        return destination;
     }
 }
